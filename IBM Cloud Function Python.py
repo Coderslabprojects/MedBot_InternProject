5407bcac53d9c166bb095909a36e484cc061e9b7
Coderslabprojects <110289055+Coderslabprojects@users.noreply.github.com>
2024-08-03 09:22:01 +0530

    IBM_Cloud_Function_Python.py

--- IBM Cloud Function Python.py
+++ IBM Cloud Function Python.py
@@ -1,12 +1,7 @@
-#
-#
-# main() will be run when you invoke this action
-#
-# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-#
+# The main() function will execute when this action is invoked
+# @param Cloud Functions actions require a single parameter, which must be provided as a JSON object.
 # @return The output of this action, which must be a JSON object.
-#
-#
+
 import sys
 
 def main(args):
